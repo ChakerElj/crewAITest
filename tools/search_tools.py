03bba8a46cb7d6ae893198fba332b86890d59624
ChakerElj <eljchaker232@gmail.com>
2024-06-10 05:36:26 +0100

    last commit

--- tools/search_tools.py
+++ tools/search_tools.py
@@ -8,9 +8,11 @@ from langchain.tools import tool
 class SearchTools():
     @tool("Search the internet")
     def search_internet(query):
+        """Useful to search the internet
+                about a a given topic and return relevant results"""
         top_result_to_return = 4
         url = "https://google.serper.dev/search"
-        payload = json.dump({"q":query})
+        payload = json.dumps({"q":query})
         headers = {
             'X-API-KEY' : os.environ['SERPER_API_KEY'],
             'Content-type' : 'application/json'
